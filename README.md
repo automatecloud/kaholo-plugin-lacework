5dd9fc036dc9756ad766ebd1f48eccb28c7032be
Amit Alon <amit@kaholo.io>
2021-03-02 20:40:15 +0200

    Removed GetToken from public plugin methods, and changed request to node-fetch

--- README.md
+++ README.md
@@ -2,18 +2,12 @@
 Kaholo Plugin for Lacework
 
 ### Settings:
-Secret: Lacework secret
-URL: The base URL to your Lacework environment.
-Key Id: Lacework Key ID
+1) Secret(required) - Lacework secret access key. Can only be created by administrator using the Lacework console.
+2) URL(required) - The base URL to your Lacework environment.
+3) Key Id(required) - Lacework access Key ID. Can only be created by administrator using the Lacework console.
 
 ## Method: Get event details
-This method will return all the data from event which lasework have.
+This method returns all the data about the lacework event specified.
 
 ### Parameters:
-The event Id
-
-## Method: Create token
-This method will return a token back for other API calls
-
-### Parameters:
-None
+1) Event ID(required) - The event ID of the requested event. If using the lacework webhook, you can get it from the event_id field.
--- src/app.js
+++ src/app.js
@@ -1,67 +1,50 @@
-const request = require('request');
+const fetch = require('node-fetch');
 
 async function GetEventDetails(action, settings) {
-    let response = await createToken(action, settings)
-    const token = response.body.data[0].token;
-    const eventID = action.params.EVENT_ID;
-    const url = `${settings.HOST}/api/v1/external/events/GetEventDetails?EVENT_ID=${eventID}`;
-    const method = "GET";
-    let options = {
-        method: method,
-        url: url
-    } 
-    //return await genericRestAPI(options, token);
-    return await genericRestAPI(options, token);
+    let host = (settings.HOST || '').trim();
+    const keyId = (settings.KEY_ID || '').trim();
+    const secret = (settings.SECRET || '').trim();
+    const eventID = (action.params.EVENT_ID || '').trim();
+    if (!host || !keyId || !secret || !eventID){ // check if all required parameters were provided
+        throw "not all required parameters and settings were provided";
+    }
+    // fix host to be in https://{lacework_account_url}.lacework.net in case it's not already
+    host = `${host.startsWith('http') ? '' : 'https://'}${host}`
+    host += host.endsWith('.lacework.net') ? '' : '.lacework.net';
+
+    const token = await getToken(host, keyId, secret);
+    
+    const url = `${host}/api/v1/external/events/GetEventDetails?EVENT_ID=${eventID}`;
+    const fetchParams = { 'headers': { 'Authorization': `Bearer ${token}` } };
+
+    return await fetchJson(url, fetchParams);
 }
 
-function createToken(action, settings) {
-    const URL = `${settings.HOST}/api/v1/access/tokens`
-    let mheaders = {
-        "Content-Type": "application/json",
-        "X-LW-UAKS": `${settings.SECRET}`
-    }
-    let mbody = {
-        "keyId": `${settings.KEY_ID}`,
-        "expiryTime": 3600
-    }
-    const requestOptions = {
-        url: URL,
-        method: "POST",
-        body: mbody,
-        json: true,
-        headers: mheaders
-    };
-    return new Promise((resolve, reject) => {
-        request(requestOptions, function (err, response, body) {
-            if (err) {
-                return reject(err);
-            }
-            resolve(response);
-        });
-    })
-}
+///// Helpers
 
-
-//////////// HELPERS ////////////
-
-async function genericRestAPI(options, token) {
-    Object.assign (options,{
-        'json':true,
-        'headers': {
-            'Authorization': `Bearer ${token}`
+async function getToken(host, keyId, secret) {
+    const url = `${host}/api/v1/access/tokens`;
+    const fetchParams = {
+        method: 'post',
+        body: JSON.stringify({
+            keyId: keyId,
+            expiryTime: 3600
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            'X-LW-UAKS': secret
         }
-    });
-    return new Promise((resolve, reject) => {
-        request(options, function (error, response) {
-            if (error) {
-                return reject(err);
-            }
-            resolve(response.body);
-        });
-    });
+    }
+    const body = await fetchJson(url, fetchParams);
+    
+    return body.data[0].token;
 }
 
-
-module.exports = {
-    GetEventDetails
+async function fetchJson(url, fetchParams){
+    const response = await fetch(url, fetchParams);
+    return response.json();
 }
+
+///// Exports
+
+module.exports = { GetEventDetails }
