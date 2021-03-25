9b69329e7a121a1055cb7ca923f8d85b6b187ecd
Andreas Wilke <andiwilke@gmail.com>
2021-03-25 15:58:30 +0100

    Updated the plugin to support AWS Reports

--- src/app.js
+++ src/app.js
@@ -10,8 +10,26 @@ async function GetEventDetails(action, settings) {
     }
 
     const token = await getToken(host, keyId, secret);
-    
+
     const url = `https://${host}.lacework.net/api/v1/external/events/GetEventDetails?EVENT_ID=${eventID}`;
+    const fetchParams = { 'headers': { 'Authorization': `Bearer ${token}` } };
+
+    return await fetchJson(url, fetchParams);
+}
+
+async function GetLatestAWSComplianceReportDetails(action, settings) {
+    const host = (settings.HOST || '').trim();
+    const keyId = (settings.KEY_ID || '').trim();
+    const secret = (settings.SECRET || '').trim();
+    const aws_account_id = (action.params.AWS_ACCOUNT_ID || '').trim();
+    const report_type = (action.params.REPORT_TYPE || '').trim();
+    if (!host || !keyId || !secret || !aws_account_id){ // check if all required parameters were provided
+        throw "not all required parameters and settings were provided";
+    }
+
+    const token = await getToken(host, keyId, secret);
+
+    const url = `https://${host}.lacework.net/api/v1/external/compliance/aws/GetLatestComplianceReport?AWS_ACCOUNT_ID=${aws_account_id}&REPORT_TYPE=${report_type}&FILE_FORMAT=json`;
     const fetchParams = { 'headers': { 'Authorization': `Bearer ${token}` } };
 
     return await fetchJson(url, fetchParams);
@@ -33,7 +51,7 @@ async function getToken(host, keyId, secret) {
         }
     }
     const body = await fetchJson(url, fetchParams);
-    
+
     return body.data[0].token;
 }
 
@@ -44,4 +62,7 @@ async function fetchJson(url, fetchParams){
 
 ///// Exports
 
-module.exports = { GetEventDetails }
+module.exports = {
+  GetEventDetails,
+  GetLatestAWSComplianceReportDetails
+}
